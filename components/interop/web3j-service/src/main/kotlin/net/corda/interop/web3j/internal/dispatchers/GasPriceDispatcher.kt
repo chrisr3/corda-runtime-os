14a90ae771cd3f83686b8887adcad432d004ada8
Ignace Loomans <ignace.loomans@gmail.com>
2023-08-23 16:36:52 +0100

    Added documentation to generic quorum dispatchers

--- components/interop/web3j-service/src/main/kotlin/net/corda/interop/web3j/internal/dispatchers/GasPriceDispatcher.kt
+++ components/interop/web3j-service/src/main/kotlin/net/corda/interop/web3j/internal/dispatchers/GasPriceDispatcher.kt
@@ -4,6 +4,12 @@ import net.corda.data.interop.evm.EvmRequest
 import net.corda.data.interop.evm.EvmResponse
 import net.corda.interop.web3j.EvmDispatcher
 import net.corda.interop.web3j.internal.EthereumConnector
+
+/**
+ * Dispatcher used to get the Gas Price.
+ *
+ * @param evmConnector The evmConnector class used to make rpc calls to the node
+ */
 class GasPriceDispatcher(val evmConnector: EthereumConnector): EvmDispatcher {
     override fun dispatch(evmRequest: EvmRequest): EvmResponse {
         val resp = evmConnector.send(evmRequest.rpcUrl, "eth_gasPrice", emptyList<String>())
